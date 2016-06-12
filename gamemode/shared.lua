7ab8ce6a09ff92e1c2a8188ab5dee798bfce5ede
whuppo <whuppo@whuppo.com>
2016-06-11 21:37:00 -0700

    added scoreboard

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -9,5 +9,9 @@ game.AddAmmoType( { name = "Rifle", dmgtype = DMG_BULLET } )
 game.AddAmmoType( { name = "Sniper", dmgtype = DMG_BULLET } )
 game.AddAmmoType( { name = "Buckshot", dmgtype = DMG_BULLET } )
 
+team.SetUp( 0, "Spectators", Color( 0, 0, 0 ) )
+team.SetUp( 1, "Red", Color( 255, 0, 0 ) )
+team.SetUp( 2, "Blue", Color( 0, 0, 255 ) )
+
 function GM:Initialize()
 end
