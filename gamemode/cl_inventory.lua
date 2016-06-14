0afbe27b97973d857523f9295bbac86517fbfec7
whuppo <whuppo@whuppo.com>
2016-06-13 20:39:07 -0700

    works! but ugly af

--- gamemode/cl_inventory.lua
+++ gamemode/cl_inventory.lua
@@ -36,36 +36,110 @@ function InventoryMenu()
 	inventorylist:SetSpaceY( ScrW() * 0.05 )
 	inventorylist:SetSpaceX( ScrW() * 0.05 )
 
-	for i = 1, 50 do
-		local item = inventorylist:Add( "DButton" )
-		item:SetSize( 256, 512 )
-		item:SetText( "" )
+	net.Start( "RequestWeapons" )
+	net.SendToServer()
+	net.Receive( "RequestWeaponsCallback", function()
+		local curr = net.ReadTable()
+		local weps = net.ReadTable()
 
-		item.name   = "Default Weapon"
-		item.rarity = math.random( 1, 5 )
-		function item:Paint()
-			surface.SetDrawColor( 0, 0, 0, 200 )
-			surface.DrawRect( 0, 0, item:GetSize() )
+		for k, v in pairs( weps ) do
+			local item = inventorylist:Add( "DButton" )
+			item:SetSize( 256, 512 )
+			item:SetText( "" )
 
-			surface.SetDrawColor( raritycol[ item.rarity ] )
-			surface.DrawRect( 0, item:GetTall() / 2, item:GetWide(), 42 )
+			item.name   = v.name
+			item.rarity = v.rarity
+			item.class  = v.class
+			function item:Paint()
+				surface.SetDrawColor( 0, 0, 0, 200 )
+				surface.DrawRect( 0, 0, item:GetSize() )
 
-			for i = 0, 4 do
-				draw.SimpleText( item.name, "ItemNameBG", item:GetWide() / 2, item:GetWide() + ( 42 / 2 ), Color( 0, 0, 0, 255 ), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER )
+				surface.SetDrawColor( raritycol[ item.rarity ] )
+				surface.DrawRect( 0, item:GetTall() / 2, item:GetWide(), 42 )
+
+				for i = 0, 4 do
+					draw.SimpleText( item.name, "ItemNameBG", item:GetWide() / 2, item:GetWide() + ( 42 / 2 ), Color( 0, 0, 0, 255 ), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER )
+				end
+				draw.SimpleText( item.name, "ItemName", item:GetWide() / 2, item:GetWide() + ( 42 / 2 ), Color( 255, 255, 255, 200 ), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER )
 			end
-			draw.SimpleText( item.name, "ItemName", item:GetWide() / 2, item:GetWide() + ( 42 / 2 ), Color( 255, 255, 255, 200 ), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER )
-		end
 
-		local model = item:Add( "DModelPanel" )
-		model:SetSize( item:GetWide(), item:GetTall() / 2 )
-		model:SetCamPos( Vector( 32, 0, 0 ) )
-		model:SetLookAt( Vector( 0, -3, 0 ) )
-		model:SetModel( "models/weapons/tfa_csgo/w_ak47.mdl" )
+			item.DoClick = function()
 
-		function model:LayoutEntity( ent )
-			ent:SetAngles( Angle( 90, 0, 90 ) )
+				itemdetail = vgui.Create( "DFrame" )
+				itemdetail:SetSize( 256, 512 )
+				itemdetail:SetTitle( "" )
+				itemdetail:SetVisible( true )
+				itemdetail:SetDraggable( false )
+				itemdetail:ShowCloseButton( true )
+				itemdetail:Center()
+				itemdetail:MakePopup()
+				function itemdetail:Init()
+					self.startTime = SysTime()
+				end
+				function itemdetail:Paint()
+					Derma_DrawBackgroundBlur( self, self.startTime )
+
+					surface.SetDrawColor( 0, 0, 0, 200 )
+					surface.DrawRect( 0, 0, itemdetail:GetSize() )
+
+					surface.SetDrawColor( raritycol[ item.rarity ] )
+					surface.DrawRect( 0, itemdetail:GetTall() / 2, itemdetail:GetWide(), 42 )
+
+					for i = 0, 4 do
+						draw.SimpleText( item.name, "ItemNameBG", itemdetail:GetWide() / 2, itemdetail:GetWide() + ( 42 / 2 ), Color( 0, 0, 0, 255 ), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER )
+					end
+					draw.SimpleText( item.name, "ItemName", itemdetail:GetWide() / 2, itemdetail:GetWide() + ( 42 / 2 ), Color( 255, 255, 255, 200 ), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER )
+				end
+
+				replacepri = itemdetail:Add( "DButton" )
+				replacepri:SetSize( itemdetail:GetWide(), 42 )
+				replacepri:SetPos( 0, itemdetail:GetTall() * ( 2/3 ) )
+				replacepri:SetText( "Replace " .. curr.primary .. " with " .. item.name )
+				replacepri.DoClick = function()
+					curr.primary = item.class
+
+					net.Start( "wz_loadout" )
+						net.WriteString( curr.primary )
+						net.WriteString( curr.secondary )
+					net.SendToServer()
+					itemdetail:Close()
+				end
+
+				replacesec = itemdetail:Add( "DButton" )
+				replacesec:SetSize( itemdetail:GetWide(), 42 )
+				replacesec:SetPos( 0, itemdetail:GetTall() * ( 2/3 ) + 44 )
+				replacesec:SetText( "Replace " .. curr.secondary .. " with " .. item.name )
+				replacesec.DoClick = function()
+					curr.secondary = item.class
+
+					net.Start( "wz_loadout" )
+						net.WriteString( curr.primary )
+						net.WriteString( curr.secondary )
+					net.SendToServer()
+					itemdetail:Close()
+				end
+
+				if curr.secondary == item.class or curr.primary == item.class then
+					replacepri:SetEnabled( false )
+					replacesec:SetEnabled( false )
+				end
+
+			end
+
+			local model = item:Add( "DModelPanel" )
+			model:SetSize( item:GetWide(), item:GetTall() / 2 )
+			model:SetCamPos( Vector( 32, 0, 0 ) )
+			model:SetLookAt( Vector( 0, -3, 0 ) )
+			model:SetModel( v.model )
+
+			-- lol it's drawn over the clickable area
+			model.DoClick = item.DoClick
+
+			function model:LayoutEntity( ent )
+				ent:SetAngles( Angle( 90, 0, 90 ) )
+			end
 		end
-	end
+	end )
 end
 
 concommand.Add( "wz_inventory", InventoryMenu )
--- gamemode/sv_inventory.lua
+++ gamemode/sv_inventory.lua
@@ -1,13 +1,39 @@
 util.AddNetworkString( "wz_loadout" )
+util.AddNetworkString( "RequestWeapons" )
+util.AddNetworkString( "RequestWeaponsCallback" )
 
 local meta = FindMetaTable( "Player" )
 
 loadouts = loadouts or {}
 preloadouts = preloadouts or {}
 
+local DEFAULTPRIMARY   = "csgo_ak47"
+local DEFAULTSECONDARY = "csgo_deagle"
+
+local defaultweps = {
+	{ name = "AK-47", class = "csgo_ak47", model = "models/weapons/tfa_csgo/w_ak47.mdl", rarity = 1 },
+	{ name = "AWP", class = "csgo_awp", model = "models/weapons/tfa_csgo/w_awp.mdl", rarity = 1 },
+	{ name = "Desert Eagle", class = "csgo_deagle", model = "models/weapons/tfa_csgo/w_deagle.mdl", rarity = 1 }
+}
+
 function GM:ShowHelp( ply )
 	ply:ConCommand( "wz_inventory" )
 end	
+
+net.Receive( "RequestWeapons", function( len, ply )
+	net.Start( "RequestWeaponsCallback" )
+		--current loadout
+		local l = loadouts[ ply ]
+		if l then
+			net.WriteTable( loadouts[ ply ] )
+		end
+		--default weapons
+		net.WriteTable( defaultweps )
+
+		--players inventory
+		--net.WriteTable( fetched_weapons )
+	net.Send( ply )
+end )
 
 net.Receive( "wz_loadout", function( len, ply )
 	local primary   = net.ReadString() // first weapon
@@ -30,9 +56,9 @@ end )
 hook.Add( "PlayerDeath", "FixLoadoutExploit", function( ply )
 	local pl = preloadouts[ ply ]
 	if pl then
-		loadout[ ply ] = {
-			primary   = primary,
-			secondary = secondary
+		loadouts[ ply ] = {
+			primary   = pl.primary,
+			secondary = pl.secondary
 		}
 	end
 end )
@@ -43,7 +69,22 @@ function meta:GiveLoadout()
 	if l then
 		self:Give( l.primary )
 		self:Give( l.secondary )
+	else
+		self:Give( DEFAULTPRIMARY )
+		self:Give( DEFAULTSECONDARY )
+
+		loadouts[ self ] = {
+			primary   = DEFAULTPRIMARY,
+			secondary = DEFAULTSECONDARY
+		}
 	end
 end
+
+hook.Add( "PlayerInitialSpawn", "SetLoadout", function( ply )
+	loadouts[ ply ] = {
+		primary   = DEFAULTPRIMARY,
+		secondary = DEFAULTSECONDARY
+	}
+end )
 
 hook.Add( "PlayerSpawn", "GiveLoadout", function( ply ) ply:GiveLoadout() end )
